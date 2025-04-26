a9a3bebdb02aba691b8b3c6c13934cc62943965f
Nisha Bukke <sononenisha827@gmail.com>
2025-04-26 18:44:09 +0530

    nirmitee developer banner change

---
+++ header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nirmitee Developers</title>
+</head>
+<body>
+<header>
+            <div id="side-menu-icon">
+                <div id="menu"><span></span></div>
+            </div>
+            <div id="side-menu">
+                <a href="index.html">Home</a>
+                <a href="about.html">About</a>
+                <a href="https://nishabukke.github.io/aarya-green-county/" target="_blank">Our Projects</a>
+                <a href="contact.html">Contact Us</a>
+            </div>
+            <div class="whatsapp-div">
+                <a href="" class="wapp-link"> <span class="fas fa-globe"></span>Visit Our Site</a>
+            </div>
+    </header>
+</body>
+</html>
